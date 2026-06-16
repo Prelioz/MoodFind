02f79cb8e18155e064aa7812ad6e72adca70df38
Prelioz <spideybhavya@gmail.com>
2026-06-16 22:24:23 +0530

    Neutral sentiment detection

--- src/predict.py
+++ src/predict.py
@@ -11,6 +11,13 @@ def predict_sentiment(review):
 
     review_vector = vectorizer.transform([review])
 
+    probabilities = model.predict_proba(review_vector)[0]
+
+    confidence = max(probabilities)
+
+    if confidence < 0.6:
+         return "Neutral"
+    
     prediction = model.predict(review_vector)[0]
 
     return "Positive" if prediction == 1 else "Negative"
@@ -18,20 +25,12 @@ def predict_sentiment(review):
 
 if __name__ == "__main__":
      
-     print(
-        predict_sentiment(
-            "This movie was absolutely amazing!"
-        )
-    )
-
-     print(
-        predict_sentiment(
-            "Worst movie I have ever watched."
-        )
-    )
+   
      print(predict_sentiment("The acting was mediocre but the story was fantastic"))
 
      print(predict_sentiment("I regret wasting my time on this movie"))
 
      print(predict_sentiment("It was okay, not the best but not terrible"))
+
+
 
